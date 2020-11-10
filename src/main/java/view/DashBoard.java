932631e16ead09924abaf7bcf8684ae0266d99cc
Alexandre Ferreira <44810225+Alexandrefg118@users.noreply.github.com>
2020-11-10 20:20:54 -0300

    adicionado tela de login

--- src/main/java/view/DashBoard.java
+++ src/main/java/view/DashBoard.java
@@ -151,7 +151,7 @@ public class DashBoard extends javax.swing.JFrame {
                 .addContainerGap()
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
-                        .addComponent(jlbAlterarCad, javax.swing.GroupLayout.PREFERRED_SIZE, 129, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(jlbAlterarCad, javax.swing.GroupLayout.PREFERRED_SIZE, 264, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addComponent(jLbLogoff)
                         .addGap(37, 37, 37))
